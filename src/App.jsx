aa20a1258420678a1ebb16fb11e06f8c793cdc59
Hao <hao-le@live.co.uk>
2022-12-27 16:48:43 +0000

    inserted api key and implementing api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { getLocation } from "./currentLocation";
+import apiKeys from "./apiKeys";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [weather, setWeather] = useState({});
+  const getWeather = async (lat, lon) => {
+    const response = await fetch(
+      `${apiKeys.base}current.json?key=${apiKeys.key}&q=${lat},${lon}&aqi=no`
+    );
+    const weather = await response.json();
+    return setWeather(weather);
+  };
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
     <div>
       <h1>hello world</h1>
     </div>
   );
-}
+};
 
 export default App;
