653e09ed83b5d521cbe46637ea14eebfc1743aa6
Hao <hao-le@live.co.uk>
2022-12-28 09:57:09 +0000

    moved content to Home, added img and info

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,10 @@
-import "./App.css";
-
-import { useEffect, useState } from "react";
+import "./App.scss";
+import Home from "./Pages/Home/Home";
 
 const App = () => {
-  const [weather, setWeather] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  const getWeather = async (latitude, longitude) => {
-    const response =
-      await fetch(`http://api.weatherapi.com/v1/current.json?key=326cc5d6256e4eae995154922222712&q=${latitude},${longitude}&aqi=no
-    `);
-    const data = await response.json();
-    setWeather(data);
-    console.log(data);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        getWeather(position.coords.latitude, position.coords.longitude);
-      });
-    }
-  }, []);
-
   return (
     <div>
-      <h1>hello world</h1>
-      {!loading && !weather.error && (
-        <h1>
-          {weather.location.name} {weather.current.temp_c}
-        </h1>
-      )}
+      <Home />
     </div>
   );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,44 @@
 import "./Home.scss";
+import apiKeys from "../../apiKeys";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  return <h1>hello hi</h1>;
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const getWeather = async (latitude, longitude) => {
+    const response =
+      await fetch(`${apiKeys.base}/current.json?key=${apiKeys.key}&q=${latitude},${longitude}&aqi=no
+    `);
+    const data = await response.json();
+    setWeather(data);
+    console.log(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        getWeather(position.coords.latitude, position.coords.longitude);
+      });
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>hello world</h1>
+      {!loading && !weather.error && (
+        <h1>
+          {weather.location.name} {weather.current.temp_c}°C{" "}
+          {weather.current.condition.text}
+          <img
+            src={weather.current.condition.icon}
+            alt={weather.current.condition.text}
+          />
+        </h1>
+      )}
+    </div>
+  );
 };
 
 export default Home;
