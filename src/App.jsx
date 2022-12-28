e58cf22f43f10c56f68e57b8b1a011fbfd30ebed
Hao <hao-le@live.co.uk>
2022-12-28 02:29:46 +0000

    forecase 400 bad request error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,35 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [longitude, setLongitude] = useState([]);
-  const [latitude, setLatitude] = useState([]);
-  const [weather, setWeather] = useState({});
-  // useEffect(() => {
-  //   navigator.geolocation.getCurrentPosition(function (position) {
-  //     setLatitude(position.coords.latitude);
-  //     setLongitude(position.coords.longitude);
-  //   });
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  //   console.log("Latitude is:", latitude);
-  //   console.log("Longitude is:", longitude);
-  // }, [latitude, longitude]);
+  const getWeather = async (latitude, longitude) => {
+    const response =
+      await fetch(`http://api.weatherapi.com/v1/current.json?key=326cc5d6256e4eae995154922222712&q=${latitude},${longitude}&aqi=no
+    `);
+    const data = await response.json();
+    setWeather(data);
+    console.log(data);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    const getWeather = async () => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-          setLatitude(position.coords.latitude);
-          setLongitude(position.coords.longitude);
-        });
-        console.log("Latitude is:", latitude);
-        console.log("Longitude is:", longitude);
-      } else {
-        alert("Sorry Not available!");
-      }
-      const response = await fetch(
-        `http://api.weatherapi.com/v1/current.json?key=326cc5d6256e4eae995154922222712&q=${latitude},${longitude}&aqi=no
-      `
-      );
-      const data = await response.json();
-      setWeather(data);
-      console.log(data);
-    };
-    getWeather();
-  }, [latitude, longitude]);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        getWeather(position.coords.latitude, position.coords.longitude);
+      });
+    }
+  }, []);
 
   return (
     <div>
       <h1>hello world</h1>
-      {/* <h1>{weather.location.name}</h1>
-      <h1>{weather.location.country}</h1>
-      <h1>{weather.current.temp_c}°C</h1> */}
+      {!loading && !weather.error && (
+        <h1>
+          {weather.location.name} {weather.current.temp_c}
+        </h1>
+      )}
     </div>
   );
 };
